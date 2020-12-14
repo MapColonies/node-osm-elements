29c6ab3e7cc9c9a9a26befb00d6833d7c7e8bd47
galta95 <48960890+galta95@users.noreply.github.com>
2020-12-14 09:43:36 +0200

    chore: Init skeleton (#7)
    * ci: added lint github action
    
    * build(dependencies): added devDependencies
    
    * style: added commitlint and  prettier
    
    * chore: added typescript configurations
    
    * style: run prettier
    
    * chore: added folder hierarchy
    
    * chore: added eslint configuration
    
    * style: added commit lint scope enums
    
    * chore(configurations): added organization  to the package name

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [2, "always", ["node", "way", "relation", "changeset", "osmChange", "dependencies", "configurations"]],
+  },
+};
