ac58085ee4b7b4ad2af634b94473a6263a40aea4
amito <samit2310@gmail.com>
2021-01-24 15:49:13 +0200

    build(deps): updated eslint config version

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [2, "always", ["node", "way", "relation", "changeset", "osmChange", "dependencies", "configurations"]],
+    "scope-enum": [2, "always", ["util", "node", "way", "relation", "changeset", "osmChange", "deps", "configurations"]],
   },
 };
